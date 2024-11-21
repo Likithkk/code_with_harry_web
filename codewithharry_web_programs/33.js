a38395ad79dd796d388bd527a529709eec29bd11
Likithkk <123310091+Likithkk@users.noreply.github.com>
2024-11-21 15:07:23 +0530

    Practice codes

---
+++ codewithharry_web_programs/33.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+function print(name){
+    console.log(name+"is a name");
+}
+
+let name = "XYZ";
+let name1 = "XYZ1";
+let name2 = "XYZ2";
+print(name);
+print(name1);
+print(name2);
+// console.log(name +"is a name");
+// console.log(name1+"is a name");
+// console.log(name2+"is a name");
